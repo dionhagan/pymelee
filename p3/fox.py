90c1b8acec6458a9b2f8c6c6dec7a397779c8f0e
Dion Hagan <dionhagan@college.harvard.edu>
2016-10-03 15:15:03 -0400

    added moves to moveset

--- p3/fox.py
+++ p3/fox.py
@@ -16,10 +16,106 @@ class Fox:
             self.last_action = state.frame
         else:
             # Eventually this will point at some decision-making thing.
-            # self.shinespam(pad)
-            for x in range(10):
-                self.sh_laser(pad)
+            # pseudo: self.action_list = max(score(getSuccessors()))
+            print (state.players)
+            print ('\n')
+            self.pressB(pad)
+            self.shorthop_laser(pad)
+            self.pressB(pad, 0.5, 1)
 
+    """
+        FOX MOVESET
+    """
+
+    # FUNDAMENTALS
+    def stop(self):
+        self.action_list.append((1, None, []))
+
+    def pressA(self, pad, x=0.5, y=0.5):
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, x, y]))
+        self.action_list.append((0, pad.press_button, [p3.pad.Button.A]))
+        self.action_list.append((2, pad.release_button, [p3.pad.Button.A]))
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.5]))
+
+    def pressB(self, pad, x=0.5, y=0.5):
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, x, y]))
+        self.action_list.append((0, pad.press_button, [p3.pad.Button.B]))
+        self.action_list.append((2, pad.release_button, [p3.pad.Button.B]))
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.5]))
+
+    def pressX(self, pad):
+        self.action_list.append((0, pad.press_button, [p3.pad.Button.X]))
+        self.action_list.append((2, pad.release_button, [p3.pad.Button.X]))
+
+    def shield(self, pad):
+        self.action_list.append((0, pad.press_trigger, [p3.pad.Trigger.L, 1]))
+
+
+    # MOVEMENT
+    def jump(self, pad):
+        self.action_list.append((0, pad.press_button, [p3.pad.Button.X]))
+        self.action_list.append((3, pad.release_button, [p3.pad.Button.X]))
+
+    def shorthop(self, pad):
+        self.action_list.append((0, pad.press_button, [p3.pad.Button.X]))
+        self.action_list.append((1, pad.release_button, [p3.pad.Button.X]))
+
+    def double_jump(self, pad):
+        self.jump()
+        self.jump()
+
+    def dash(self, pad, x, dur):
+        self.action_list.append((0, pad.tilt_stick, x, 0.5))
+        self.action_list.append((dur, pad.tilt_stick, 0.5, 0.5))
+
+    # SPECIALS
+    def laser(self, pad):
+        self.action_list.append((0, pad.press_button, [p3.pad.Button.B]))
+        self.action_list.append((1, pad.release_button, [p3.pad.Button.B]))
+
+    def upB(self, pad):
+        pass
+
+    def sideB(self, pad):
+        pass
+
+    def shine(self, pad):
+        pass
+
+    # SMASH ATTACKS
+    def fsmash(self, pad, x):
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.C, x, 0.5]))
+        self.action_list.append((1, pad.tilt_stick, [p3.pad.Stick.C, 0.5, 0.5]))
+
+    def up_smash(self, pad):
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.C, 0.5, 1]))
+        self.action_list.append((1, pad.tilt_stick, [p3.pad.Stick.C, 0.5, 0.5]))
+
+    def down_smash(self, pad):
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.C, 0, 0.5]))
+        self.action_list.append((1, pad.tilt_stick, [p3.pad.Stick.C, 0.5, 0.5]))
+
+    # AERIALS
+    def shffl_nair(self, pad):
+        self.shorthop(pad)
+        self.pressA(pad)
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0]))
+        self.action_list.append((1, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.5]))
+
+    def shffl_fair(self, pad):
+        self.shorthop(pad)
+        self.pressA(pad,)
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0]))
+        self.action_list.append((1, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.5]))
+
+    def shffl_dair(self, pad):
+        pass
+
+    def shffl_bair(self, pad):
+        pass
+
+
+    # TECH
     def shinespam(self, pad):
         self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.0]))
         self.action_list.append((0, pad.press_button, [p3.pad.Button.B]))
@@ -29,11 +125,18 @@ class Fox:
         self.action_list.append((1, pad.release_button, [p3.pad.Button.X]))
         self.action_list.append((1, None, []))
 
-    def sh_laser(self, pad):
+    def shorthop_laser(self, pad):
         self.action_list.append((0, pad.press_button, [p3.pad.Button.X]))
-        self.action_list.append((1, pad.release_button, [p3.pad.Button.X]))
+        self.action_list.append((2, pad.release_button, [p3.pad.Button.X]))
         self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.5]))
         self.action_list.append((0, pad.press_button, [p3.pad.Button.B]))
         self.action_list.append((1, pad.release_button, [p3.pad.Button.B]))
         self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0]))
         self.action_list.append((1, None, []))
+
+    def wavedash(self, pad, x):
+        self.shorthop(pad)
+        self.action_list.append((0, pad.tilt_stick, [p3.pad.Stick.MAIN, x, 0]))
+        self.action_list.append((0, pad.press_trigger, [p3.pad.Trigger.L]))
+        self.action_list.append((0, pad.release_trigger, [p3.pad.Trigger.L]))
+        self.action_list.append((1, pad.tilt_stick, [p3.pad.Stick.MAIN, 0.5, 0.5]))
