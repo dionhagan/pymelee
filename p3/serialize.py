09fd535d0282a3620a7c32171c8ab465c9ca086e
Dion Hagan <dionhagan@college.harvard.edu>
2016-11-30 18:35:07 -0500

    removed serialize.py

--- p3/serialize.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class Serializer:
-    def __init__(self, obj):
-        self.obj = obj
-    def toJSON(self):
-        return json.dumps(self.obj, default=lambda o: o.__dict__,
-            sort_keys=True, indent=4)
